4552832f66d3884c458fffbf2dc06da355a8f044
Jima Victor <jimavictor1@gmail.com>
2022-03-04 03:53:39 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+This is a solution to the [E-commerce product page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ecommerce-product-page-UPsZ9MJp6). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Open a lightbox gallery by clicking on the large product image
+- Switch the large product image by clicking on the small thumbnail images
+- Add items to the cart
+- View the cart and remove items from it
+
+### Screenshot
+
+![mobile-view](./images/screenshot1.png)
+![desktop-view](./images/screenshot2.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- Javascript
+
+### What I learned
+
+- Css positioning:
+  How to use position: relative on a parent element and position: absolute on the element to be positioned.
+- Css Filter:
+  How to use filter properties to set the color of images
---
+++ script.js
@@ -0,0 +1,119 @@
+var menu = document.getElementById("menu-icon");
+var overlay = document.getElementById("overlay");
+var addToCartButton = document.getElementById("add-to-cart");
+var nav = document.querySelector(".side-bar");
+var lowerSection = document.querySelector(".lower-section");
+var emptyCartMessage = document.querySelector(".empty-cart");
+var cancel = document.querySelector(".close-icon");
+var cart = document.querySelector("#shopping-cart");
+var cartItems = document.querySelector(".cart-items");
+var itemCount = document.querySelector("#item-count");
+var increaseButton = document.getElementById("increase");
+var leftButton = document.getElementById("left-button");
+var rightButton = document.getElementById("right-button");
+var leftarrow = document.getElementById("left-arrow");
+var rightarrow = document.getElementById("right-arrow");
+var decreaseButton = document.getElementById("decrease");
+var NumberOfItems = document.getElementById("number-of-items");
+var amount = document.getElementById("amount");
+var productDesktop = document.getElementById("main-product-desktop");
+var product = document.getElementById("main-product");
+var productLightBox = document.getElementById("main-product-light-box");
+var deleteIcon = document.getElementById("icon-delete");
+var NumberOfItemsAdded = document.getElementById("number-of-items-added");
+var imageOverlayLightBox = document.querySelectorAll(".img-overlay-light-box");
+var lightBox = document.querySelector(".light-box");
+var closeIconLightBox = document.getElementById("close-icon-light-box");
+
+var imageOverlay = document.querySelectorAll(".img-overlay");
+var num = 1;
+
+// NAVIGATION
+menu.addEventListener("click", showMenu);
+cancel.addEventListener("click", cancelButton);
+
+// CART
+cart.addEventListener("click", showCart);
+
+// BUTTONS
+increaseButton.addEventListener("click", increase);
+decreaseButton.addEventListener("click", decrease);
+
+addToCartButton.addEventListener("click", addToCart);
+deleteIcon.addEventListener("click", deleteProduct);
+closeIconLightBox.addEventListener("click", removeLightBox);
+
+leftButton.addEventListener("click", leftMove);
+leftarrow.addEventListener("click", leftMove);
+
+rightarrow.addEventListener("click", rightMove);
+rightButton.addEventListener("click", rightMove);
+
+// OVERLAYS FOR THUMBNAILS
+imageOverlay.forEach(imageOverlayProduct);
+imageOverlayLightBox.forEach(imageOverlayLightBoxProduct);
+
+productDesktop.addEventListener("click", openLightBox);
+// FUNCTIONS
+function imageOverlayProduct(thumbnail, index) {
+  thumbnail.addEventListener("click", function () {
+    productDesktop.src = `./images/image-product-${index + 1}.jpg`;
+    productLightBox.src = `./images/image-product-${index + 1}.jpg`;
+  });
+}
+function imageOverlayLightBoxProduct(thumbnail, index) {
+  thumbnail.addEventListener("click", function () {
+    productDesktop.src = `./images/image-product-${index + 1}.jpg`;
+    productLightBox.src = `./images/image-product-${index + 1}.jpg`;
+  });
+}
+function addToCart() {
+  if (NumberOfItems.innerHTML != 0) {
+    itemCount.innerHTML = NumberOfItems.innerHTML;
+    itemCount.style.display = "inline";
+    emptyCartMessage.style.display = "none";
+    lowerSection.style.display = "flex";
+    NumberOfItemsAdded.innerHTML = NumberOfItems.innerHTML;
+    amount.innerHTML =
+      " " + "$" + parseInt(NumberOfItemsAdded.innerHTML) * 125 + ".00";
+  }
+}
+function showCart() {
+  cartItems.classList.toggle("toggle-show");
+}
+function deleteProduct() {
+  itemCount.style.display = "none";
+  emptyCartMessage.style.display = "flex";
+  lowerSection.style.display = "none";
+}
+function increase() {
+  NumberOfItems.innerHTML = parseInt(NumberOfItems.innerHTML) + 1;
+}
+function decrease() {
+  if (NumberOfItems.innerHTML != 0)
+    NumberOfItems.innerHTML = parseInt(NumberOfItems.innerHTML) - 1;
+}
+function rightMove() {
+  if (num < 4) num++;
+  productLightBox.src = `./images/image-product-${num}.jpg`;
+  product.src = `./images/image-product-${num}.jpg`;
+}
+function leftMove() {
+  if (num > 1) num--;
+  product.src = `./images/image-product-${num}.jpg`;
+  productLightBox.src = `./images/image-product-${num}.jpg`;
+}
+function cancelButton() {
+  nav.style.display = "none";
+  overlay.classList.remove("overlay");
+}
+function showMenu() {
+  nav.style.display = "block";
+  overlay.classList.add("overlay");
+}
+function removeLightBox() {
+  lightBox.style.display = "none";
+}
+function openLightBox() {
+  lightBox.style.display = "flex";
+}
